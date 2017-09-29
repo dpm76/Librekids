aae0d7476db6724585020a1fdcf738c152e2ce74
dpm76 <davidpm.itengineer@gmail.com>
2017-09-29 21:17:04 +0200

    Layout

--- source/librekids/core/views.py
+++ source/librekids/core/views.py
@@ -9,7 +9,67 @@ class IndexView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
-        #context['latest_articles'] = Article.objects.all()[:5]
+        context["main_menu"] = [           
+            {"label": "Home"},
+            {"label": "Kids"},
+            {"label": "Kindergarten"},
+            {"label": "Portfolios"},
+            {"label": "Messages"},
+            {"label": "Projects"},
+            {"label": "Equipment"}, 
+        ]
+                
+        context["user_menu"] = [
+            {"label": "Profile"},
+            {"label": "My kids"},
+            {"label": "My portfolios"},
+            {"label": "My classrrom"},
+            {"label": "About"},
+            {"label": "Logout"},
+        ]
+        
+        context["context_panel"] = {
+            "label": "Portfolio",
+            "context_menu": [
+                {"label": "see portfolio"},
+                {"label": "new report"},
+                {"label": "send message to fathers"},
+                {"label": "see authorized persons"},
+                {"label": "new authorized person"},
+            ]
+        }
+        
+        context["breadcrumbs"] = [
+            {"label" : "home"},
+            {"label" : "Pepito Pérez"},
+            {"label" : "portfolio"},            
+        ]
+        
+        context["user_notifications"] = [
+            {
+                "label": "Messages",
+                "name": "messages",
+                "status_type": "integer",
+                "status_value": "2"
+            },
+            {
+                "label": "Activity",
+                "name": "activity-stream",
+                "status_type": "boolean",
+                "status_value": "true"
+            },
+            {
+                "label": "Custom",
+                "name": "custom",
+                "status_type": "boolean",
+                "status_value": "false"
+            },
+        ]
+        
+        context["user_data"] = {
+            "label": "User name",
+        }
+                    
         return context
 
 
